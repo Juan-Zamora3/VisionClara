68f33a0f43c3b8ff195e598f6ca41038f508aa6a
unknown <israelortizp31@gmail.com>
2025-09-23 22:16:05 -0600

    mañanalesigo

--- src/app/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Redirect } from "expo-router";
-
-// Simulación de estado de sesión
-const isLoggedIn = false;
-
-export default function Index() {
-  return <Redirect href={isLoggedIn ? "/tabs" : "/landing"} />;
-}
